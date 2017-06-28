8381a41d1221f1529b21d7d5e97510652954734c
kakts <kakts.git@gmail.com>
2017-06-29 01:56:54 +0900

    [WIP] Fix modules

--- test/lib/converter/converter.js
+++ test/lib/converter/converter.js
@@ -1,9 +1,8 @@
 const assert = require('power-assert');
 
-const util = require('../../lib/util');
-console.error('-----util', util);
-const getDataType = util.getDataType;
-describe('getDataType', () => {
+const converter = require('../../lib/converter/converter');
+console.error("---converter", converter);
+describe('converter', () => {
   it('should return string if value type is string', (done) => {
     const result = util.getDataType('test');
     assert.equal(result, 'string');
